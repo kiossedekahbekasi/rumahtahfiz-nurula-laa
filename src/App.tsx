742b80b8c5a10c3f43d64e9bd8a59f8535f82332
kiossedekahbekasi <kiossedekahbekasi@gmail.com>
2026-08-04 07:34:04 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -292,6 +292,7 @@ export default function App() {
             programs={programs}
             onSponsorSantri={handleSponsorSantri}
             onOpenRegisterForm={() => setActiveTab('pendaftaran')}
+            siteConfig={siteConfig}
           />
         )}
 
